483e70a55152c826d9f7bc085f20da4512159d49
Matt Barnfield <matt.barnfield@softwire.com>
2020-05-27 16:03:23 +0100

    Add items

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+@app.route('/add-item', methods=['POST'])
+def add_item():
+    session.add_item(request.form.get('title'))
+    return redirect('/')
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=session.get_items())
 
 if __name__ == '__main__':
     app.run()
